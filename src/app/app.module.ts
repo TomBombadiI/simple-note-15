1fa01a0f02b9359e6c7b86e3b5730e51ab2283e2
TomBombadil <sergey.bondarenko457@gmail.com>
2024-01-22 00:43:23 +0700

    fix editor

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,20 +3,9 @@ import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { NgModule } from '@angular/core';
 import { FormsModule } from '@angular/forms';
 import { HttpClientModule } from '@angular/common/http';
-import { InputTextModule } from 'primeng/inputtext';
-import { InputTextareaModule } from 'primeng/inputtextarea';
-import { InputNumberModule } from 'primeng/inputnumber';
-import { ButtonModule } from 'primeng/button';
-import { TableModule } from 'primeng/table';
-import { DialogModule } from 'primeng/dialog';
-import { ConfirmDialogModule } from 'primeng/confirmdialog';
-import { DropdownModule } from 'primeng/dropdown';
-import { RadioButtonModule } from 'primeng/radiobutton';
-import { RatingModule } from 'primeng/rating';
-import { ToolbarModule } from 'primeng/toolbar';
-import { ConfirmationService } from 'primeng/api';
-
 import { AppComponent } from './app.component';
+
+import {EditorModule} from 'primeng/editor';
 
 @NgModule({
     declarations: [
@@ -26,20 +15,12 @@ import { AppComponent } from './app.component';
         BrowserModule,
         BrowserAnimationsModule,
         FormsModule,
-        TableModule,
         HttpClientModule,
-        InputTextModule,
-        DialogModule,
-        ToolbarModule,
-        ConfirmDialogModule,
-        RatingModule,
-        InputNumberModule,
-        InputTextareaModule,
-        RadioButtonModule,
-        DropdownModule,
-        ButtonModule
+        EditorModule
     ],
-    providers: [ConfirmationService],
+    providers: [],
     bootstrap: [AppComponent]
 })
-export class AppModule { }
+export class AppModule { 
+    
+}
